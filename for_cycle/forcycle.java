bb4a5983019f0f25024056d1b4cbf12da356be06
ManuelFuego <56394536+ManuelFuego@users.noreply.github.com>
2021-08-20 12:45:58 +0000

    Add files via upload

---
+++ for_cycle/forcycle.java
@@ -0,0 +1,15 @@
+package for_cycle;
+
+public class forcycle {
+    public static void main(String args[]){
+        double x,y,z ;
+        x = 3;
+        y = 5;
+        if (x != 0 ){
+            System.out.println("x doesnt equal zero");
+            z = y / x ;
+            System.out.println("z equal "+ z );
+
+        }
+    }
+}
---
+++ for_cycle/forcycle2.java
@@ -0,0 +1,8 @@
+package for_cycle;
+
+public class forcycle2 {
+    public static void main(String args[]){
+        int x =0;
+        for(; x < 25;x++)System.out.println("current x = " + x);
+    }
+}
